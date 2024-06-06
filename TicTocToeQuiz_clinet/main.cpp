9884216d551b8895170fa1d933361497f39c0405
Alireza rohollahi <you@example.com>
2024-06-06 17:51:44 +0330

    test

--- TicTocToeQuiz_clinet/main.cpp
+++ TicTocToeQuiz_clinet/main.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    qDebug() <<"salam";
     w.show();
     return a.exec();
 }
