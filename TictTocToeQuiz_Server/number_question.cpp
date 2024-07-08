4def0caa70ad011c5a3d60b3e221a8389b967ec9
Polonium05 <155730886+Polonium05@users.noreply.github.com>
2024-07-08 14:52:48 +0330

    commited

--- TictTocToeQuiz_Server/number_question.cpp
+++ TictTocToeQuiz_Server/number_question.cpp
@@ -9,12 +9,16 @@ QJsonObject Number_Question::GetQuestion()
         "https://questionbank.liara.run/api/QWxpcmV6YSByb29ob2xsYWhpLEZhcnNoYWQgZ2hhZGFtLFk4NUZ2MnBZa2xNMA/question?type=number";
     QUrl url(Address);
     QNetworkAccessManager manager;
+    while(1)
+    {
     QNetworkReply *Reply= manager.get(QNetworkRequest(url));
     if(Reply->error()==QNetworkReply::NoError)
     {
         QByteArray data=Reply->readAll();
         QJsonDocument document=QJsonDocument::fromJson(data);
         QJsonObject json= document.object();
+        if(json["message"]=="error message here")continue;
         return json;
+    }
     }
 }
